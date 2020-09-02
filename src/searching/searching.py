9bf6b80da4e6924a8c37988cc8cf48f15b9116bf
Mikey Williams <manicdesigner@gmail.com>
2020-09-02 19:58:29 -0400

    linear_search completed

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,36 +1,39 @@
+
+arr1 = [-2, 7, 3, -9, 5, 1, 0, 4, -6]
 def linear_search(arr, target):
     # loop through the array
-    for i in arr:
+    for i in range(len(arr)):
         # compare values
-        if i == target:
-             return True
+        if arr[i] == target:
+            return i
         # else: keep looping
     # finish looping:
     # we didnt find it. return False
 
     return -1   # not found
 
+linear_search(arr1, -9)
 
 # Write an iterative implementation of Binary Search
-def binary_search(arr, target):
-    start = 0
-    end = len(arr) - 1
-    # look at middle
-    # compare to middle
-        # how get midpoint?
-            # for whole array, len(arr) / 2 - 1
-            # for subset of array: (start + end) // 2
-    while end >= start:
-        middle_index = (start + end) // 2
-        middle_value = arr[middle_index]
+# def binary_search(arr, target):
+#     start = 0
+#     end = len(arr) - 1
+#     # look at middle
+#     # compare to middle
+#         # how get midpoint?
+#             # for whole array, len(arr) / 2 - 1
+#             # for subset of array: (start + end) // 2
+#     while end >= start:
+#         middle_index = (start + end) // 2
+#         middle_value = arr[middle_index]
 
-        if target == middle_value:
-            return True
-        if target == middle_value:
-            start = middle_index + 1
-        if target < middle_value:
-            end = middle_index - 1
-
+#         if target == middle_value:
+#             return True
+#         if target == middle_value:
+#             start = middle_index + 1
+#         if target < middle_value:
+#             end = middle_index - 1
+#     return -1  # not found
 
     # if target == middle value:
         # return true
@@ -48,4 +51,4 @@ def binary_search(arr, target):
         # pass around slices of array - creates new array, use extra memory & time
 
 
-    return -1  # not found
+
