33808d1468002b5f08ac577f0a35caa06f29076c
Mikey Williams <manicdesigner@gmail.com>
2020-09-03 19:06:11 -0400

    working on

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,18 +1,7 @@
 # TO-DO: Complete the selection_sort() function below
 
 def selection_sort(arr):
-
-    for i in range(len(arr)):
-        min_index = i
-
-        for j in range(i + 1, len(arr)):
-
-            if arr[min_index] > arr[j]:
-                min_index = j
-
-        arr[i], arr[min_index] = arr[min_index], arr[i]
-
-    return arr
+    pass
 
         # TO-DO: swap
         # Your code here
